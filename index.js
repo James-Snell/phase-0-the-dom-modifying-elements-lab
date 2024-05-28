55efa7e6fc926abcddd141951364833d255ff97b
James <jmsn311@gmail.com>
2024-05-27 22:03:10 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+
+//   element.append(ul);
+  
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "James is the champion";
+newHeader.setAttribute('id', 'victory');
